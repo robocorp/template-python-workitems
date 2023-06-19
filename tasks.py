b12f8288185c9ea1914d8bac9e033e73bd5721db
Ossi R <ossi@robocorp.com>
2023-06-19 17:31:19 +0300

    Fix wrong payload key

--- tasks.py
+++ tasks.py
@@ -26,7 +26,7 @@ def consumer():
         try:
             name = item.payload["Name"]
             address = item.payload["Zip"]
-            product = item.payload["Item"]
+            product = item.payload["Product"]
             print(f"Processing order: {name}, {address}, {product}")
             item.done()
         except KeyError as err:
