dcb2cdf67afa68e4aaf2a903f022a4bd3e1193e1
Ossi Rajuvaara <ossi@robocorp.com>
2023-09-28 11:11:10 +0300

    Use proper api for reading output dir

--- tasks.py
+++ tasks.py
@@ -1,19 +1,18 @@
-import os
 from pathlib import Path
 
 from robocorp import workitems
-from robocorp.tasks import task
+from robocorp.tasks import get_output_dir, task
 from RPA.Excel.Files import Files as Excel
 
 
 @task
 def producer():
     """Split Excel rows into multiple output Work Items for the next step."""
-    artifacts = Path(os.environ["ROBOT_ARTIFACTS"])
+    output = get_output_dir() or Path("output")
     filename = "orders.xlsx"
 
     for item in workitems.inputs:
-        path = item.get_file(filename, artifacts / filename)
+        path = item.get_file(filename, output / filename)
 
         excel = Excel()
         excel.open_workbook(path)
